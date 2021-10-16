316770ff3536cd1902b072a7ff9f393e2bcc37e3
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-16 14:45:17 -0300

    import the styles & add logic to render menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,61 @@
-import React from "react";
+/** @jsxRuntime classic /
+/* @jsx jsx */
+import { jsx } from "@emotion/react";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { menuItems } from "./Navbar.constants";
+import {
+  navbarStyles,
+  navbarLogoStyles,
+  navbarMenuStyles,
+  navbarButtonStyles,
+  navbarMenuIcon,
+  sidebarStyles,
+  sidebarMenuStyles,
+  sidebarButtonLogoStyles,
+  sidebarButtonStyles,
+} from "./Navbar.styled.component";
 
-function Navbar(props) {
+const Navbar = (props) => {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+
+  const toggleSideBar = () => {
+    setNavbarIsOpen(!navbarIsOpen);
+  };
+
   return (
     <div>
-      <div>
-        <a>Aski Uru</a>
+      <div css={navbarStyles}>
+        <div>
+          <a href="#/" css={navbarLogoStyles}>Aski Uru</a>
+        </div>
+        <ul css={navbarMenuStyles}>
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index} css={navbarButtonStyles}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <div css={navbarMenuIcon} onClick={toggleSideBar}>
+          <FaBars />
+        </div>
       </div>
-      <ul>
-          <li>Inicio</li>
-          <li>Pinturas</li>
-          <li>Artistas</li>
-          <li>Nosotros</li>
-      </ul>
+      <div css={sidebarStyles({ navbarIsOpen })}>
+        <ul css={sidebarMenuStyles}>
+          <li css={sidebarButtonLogoStyles}>ASKI URU</li>
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index} css={sidebarButtonStyles}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Navbar;
