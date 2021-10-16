96e2046e5727a5b8788f7e29e3bd99293fc5b7a6
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-16 11:17:32 -0300

    create a navbar component file

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <div>
+      <div>
+        <a>Aski Uru</a>
+      </div>
+      <ul>
+          <li>Inicio</li>
+          <li>Pinturas</li>
+          <li>Artistas</li>
+          <li>Nosotros</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
