e7454cd4dc8fc314ee2c4b1fb5cf7d809964b110
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-20 01:45:38 -0300

    2-Catalogo con maps & promises: create ItemList component file

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,28 @@
-import React from 'react';
+/** @jsxRuntime classic /
+/* @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import Item from "./Item";
 
-const ItemList = (props) => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const itemListStyles = css`
+    display:flex;
+    justify-content: center;
+    flex-wrap:wrap;
+`;
+
+const ItemList = ({ products }) => {
+  return (
+    <div css={itemListStyles}>
+      {products.map((product) => (
+        <Item
+          key={product.id}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          image={product.image}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ItemList;
