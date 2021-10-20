15173c9e07646a908cb6846ed744aa0e01e44657
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-20 01:45:24 -0300

    2-Catalogo con maps & promises: create Item component file

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,42 @@
-import React from 'react';
+/** @jsxRuntime classic /
+/* @jsx jsx */
+import { jsx, css } from "@emotion/react";
 
-const Item = (props) => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const itemStyles = css`
+  width: 225px;
+  box-sizing: border-box;
+  background: #ffffff;
+  padding: 20px 20px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-bottom: 10px;
+`;
+
+const imageStyles = css`
+    width: 100%;
+    height: 150px;
+`;
+
+const nameStyles = css`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const priceStyles = css`
+  font-size: 26px;
+  font-weight: bold;
+`;
+
+const descriptionStyles = css``;
+const Item = ({ name, description, price, image }) => {
+  return (
+    <div css={itemStyles}>
+      <img css={imageStyles} src={image} alt={name} />
+      <p css={priceStyles}>{price}</p>
+      <h2 css={nameStyles}>{name}</h2>
+      <p css={descriptionStyles}>{description}</p>
+    </div>
+  );
+};
 
 export default Item;
