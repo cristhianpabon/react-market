0687b2216a77c03774154bdae02a2ee0ed08a825
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-20 00:04:12 -0300

    Create ItemListContainer to display a message

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemListContainer = (props) => {
+    return (
+        <div>
+            <p>Hola Coders!</p>
+        </div>
+    );
+}
+
+export default ItemListContainer;
