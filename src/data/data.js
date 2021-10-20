890332c024eecca2c6af551f5a4f069fd7dbb34d
Cristhian Pabon <Cristhianpabon@hotmail.com>
2021-10-20 01:43:27 -0300

    2-Catalogo con maps & promises: add local data to display products

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        id: '1',
+        name: 'Lorem ipsum amet',
+        description: 'Aenean euismod feugiat rutrum. Etiam ullamcorper orci eget erat tristique eleifend.',
+        price: '$2200',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img1.jpeg'
+    },
+    {
+        id: '2',
+        name: 'Pellentesque mi nec',
+        description: 'Vivamus at aliquet tellus, vel efficitur quam. Curabitur aliquam sagittis metus non pellentesque.',
+        price: '$4600',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img2.jpeg'
+    },
+    {
+        id: '3',
+        name: 'Pharetra eu laoreet',
+        description: 'In at nisi tortor. Duis nec commodo diam, fermentum elementum erat. Integer mi nisl, pharetra eu laoreet at, aliquet vel dolor.',
+        price: '$3500',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img3.jpeg'
+    },
+    {
+        id: '4',
+        name: 'Praesent eros diam',
+        description: 'Aenean euismod feugiat rutrum. Etiam ullamcorper orci eget erat tristique eleifend.',
+        price: '$2500',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img2.jpeg'
+    },
+    {
+        id: '5',
+        name: 'Integer nec tempor',
+        description: 'Aenean euismod feugiat rutrum. Etiam ullamcorper orci eget erat tristique eleifend.',
+        price: '$1200',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img1.jpeg'
+    },
+    {
+        id: '6',
+        name: 'Maecenas nulla neque',
+        description: 'Suspendisse tincidunt turpis vitae orci pharetra, ut tristique eros hendrerit. Aenean condimentum gravida quam id maximus.',
+        price: '$5300',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img3.jpeg'
+    },
+    {
+        id: '7',
+        name: 'Sed congue velit',
+        description: 'Aenean euismod feugiat rutrum. Etiam ullamcorper orci eget erat tristique eleifend.',
+        price: '$4300',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img3.jpeg'
+    },
+    {
+        id: '8',
+        name: 'Fusce at venenatis',
+        description: 'Praesent magna neque, pulvinar feugiat eleifend sit amet, feugiat vitae nunc. Morbi pharetra faucibus interdum.',
+        price: '$2600',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img2.jpeg'
+    },
+    {
+        id: '9',
+        name: 'Nunc nec ipsum',
+        description: 'In blandit tortor vel sapien condimentum varius. Duis a tempus metus. Donec at dolor sem. Maecenas ullamcorper auctor lacinia.',
+        price: '$5100',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img1.jpeg'
+    },
+    {
+        id: '10',
+        name: 'Cras maximus ante',
+        description: 'Aenean euismod feugiat rutrum. Etiam ullamcorper orci eget erat tristique eleifend.',
+        price: '$2500',
+        category: 'abstract',
+        stock: '2',
+        image: '/img/products/img3.jpeg'
+    }
+]
